fdcd26be5b0c190afed73c2fb6ec3169debf2626
PhoneWaiYanMoe <wyan40653@gmail.com>
2026-01-21 12:51:58 +0700

    update req

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-OWNER_TELEGRAM_ID = os.getenv('OWNER_TELEGRAM_ID')  # Your Telegram ID
+OWNER_TELEGRAM_ID = int(os.getenv('OWNER_TELEGRAM_ID')) 
 
 # Order ID Counters (in production, use database)
 ORDER_COUNTERS = {
